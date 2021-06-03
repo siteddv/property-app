608eb30b950a802b502a37b883a7cf6e3131fc61
Dimitrii Vinogradov <siteddv@gmail.com>
2021-06-03 20:14:31 +0600

    feature: added details page

---
+++ property-app/property-app/DetailsPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace property_app
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailsPage : ContentPage
+    {
+        public DetailsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GoBack(object sender, EventArgs e)
+        {
+            this.Navigation.PopAsync();
+        }
+    }
+}
