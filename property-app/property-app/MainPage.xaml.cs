7f10c6d9f10e93f0fa58ced758a2156e4a32c9c5
Dimitrii Vinogradov <siteddv@gmail.com>
2021-06-03 14:27:45 +0600

    feature: added PropertyType and Property classes

--- property-app/property-app/MainPage.xaml.cs
+++ property-app/property-app/MainPage.xaml.cs
@@ -1,12 +1,35 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
 
 namespace property_app
 {
+    [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+            this.BindingContext = this;
         }
+    }
+
+    public class PropertyType
+    {
+        public string TypeName { get; set; }
+    }
+
+    public class Property
+    {
+        public string Id => Guid.NewGuid().ToString("N");
+        public string PropertyName { get; set; }
+        public string Image { get; set; }
+        public string Address { get; set; }
+        public string Location { get; set; }
+        public string Price { get; set; }
+        public string Bed { get; set; }
+        public string Bath { get; set; }
+        public string Space { get; set; }
+        public string Details { get; set; }
     }
 }
